d0c18ed8b79544905d98977e9f3fb55b689f0133
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-08 14:04:56 +0200

    feat: i18n FR/EN translation + bump to 1.1.0

--- screen.lua
+++ screen.lua
@@ -24,7 +24,7 @@ local Size           = require("ui/size")
 local UIManager      = require("ui/uimanager")
 local VerticalGroup  = require("ui/widget/verticalgroup")
 local VerticalSpan   = require("ui/widget/verticalspan")
-local _              = require("gettext")
+local _              = require("i18n")
 local T              = require("ffi/util").template
 
 local KillerSudokuBoardWidget = lrequire("board_widget")
@@ -164,18 +164,13 @@ function KillerSudokuScreen:buildLayout()
             right_panel,
         }
     else
-        self.layout = VerticalGroup:new{
+        local content = VerticalGroup:new{
             align = "center",
-            VerticalSpan:new{ width = Size.span.vertical_large },
-            top_buttons,
-            VerticalSpan:new{ width = Size.span.vertical_large },
             board_frame,
             VerticalSpan:new{ width = Size.span.vertical_large },
             self.status_text,
-            VerticalSpan:new{ width = Size.span.vertical_large },
-            keypad,
-            VerticalSpan:new{ width = Size.span.vertical_large },
         }
+        self:buildPortraitLayout(top_buttons, content, keypad)
     end
     self[1] = self.layout
     self:ensureShowButtonState()
