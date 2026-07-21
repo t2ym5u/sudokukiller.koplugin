67f869b8eb99403af87f12d73d0f9c5b88b5911b
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-21 16:28:00 +0200

    chore: bump version to 1.2.8

--- _meta.lua
+++ _meta.lua
@@ -4,5 +4,5 @@ return {
     name = "killer_sudoku",
     fullname = _([[Killer Sudoku]]),
     description = _([[Play touch-friendly Killer Sudoku puzzles with caged sums, resume later, and reveal the solution when needed.]]),
-    version = "1.2.7",
+    version = "1.2.8",
 }
