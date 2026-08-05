7cfce9d97930a8d7fd9f69b4536d6feafd25fd64
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 14:59:33 +0200

    feat: add ES and DE translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,7 +1,7 @@
 return {
-    ["Diff: %1 (%2)"]       = { fr = "Difficulté : %1 (%2)" },
-    ["%1,%2  \xC2\xB7  %3 empty%4"]          = { fr = "%1,%2  ·  %3 vides%4" },
-    ["%1 (%2 cages)"] = { fr = "%1 (%2 cages)" },
-    ["Killer Sudoku"] = { fr = "Sudoku Killer" },
-    ["  \xC2\xB7  Cage %1/%2, sum %3/%4"] = { fr = "  ·  Cage %1/%2, somme %3/%4" },
+    ["Diff: %1 (%2)"]       = { fr = "Difficulté : %1 (%2)", es = "Dificultad: %1 (%2)", de = "Schwierigkeit: %1 (%2)" },
+    ["%1,%2  \xC2\xB7  %3 empty%4"]          = { fr = "%1,%2  ·  %3 vides%4", es = "%1,%2  ·  %3 vacías%4", de = "%1,%2  ·  %3 leer%4" },
+    ["%1 (%2 cages)"] = { fr = "%1 (%2 cages)", es = "%1 (%2 jaulas)", de = "%1 (%2 Käfige)" },
+    ["Killer Sudoku"] = { fr = "Sudoku Killer", es = "Sudoku Killer", de = "Killer-Sudoku" },
+    ["  \xC2\xB7  Cage %1/%2, sum %3/%4"] = { fr = "  ·  Cage %1/%2, somme %3/%4", es = "  ·  Jaula %1/%2, suma %3/%4", de = "  ·  Käfig %1/%2, Summe %3/%4" },
 }
