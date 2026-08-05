eca06e9b6743f19fc819387640f5e983c41a99e9
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 15:07:25 +0200

    chore: bump version to 1.2.17

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ local _ = require("gettext")
 return {
     fullname = _([[Killer Sudoku]]),
     description = _([[Play touch-friendly Killer Sudoku puzzles with caged sums, resume later, and reveal the solution when needed.]]),
-    version = "1.2.16",
+    version = "1.2.17",
 }
